cc7e26b9012f8804587852c9a5f0281a639f67dc
Pedro Henrique <ph.barbosilva@gmail.com>
2020-05-25 15:47:36 -0300

    refactor: query param crypto symbol endpoint

--- src/pages/api/crypto/symbol.ts
+++ src/pages/api/crypto/symbol.ts
@@ -4,7 +4,8 @@ let url = 'https://finnhub.io/api/v1/crypto/symbol'
 let API_KEY = process.env.FINNHUB_API_KEY
 
 export default async function handler(req, res) {
-  let data = await get(`${url}?exchange=${req.query.exchange}&token=${API_KEY}`)
+  let { exchange } = req.query
+  let data = await get(`${url}?exchange=${exchange}&token=${API_KEY}`)
 
   res.json(data)
 }
